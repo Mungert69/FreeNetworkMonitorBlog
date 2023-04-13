d2df4f9635b6d5f6fd4b0d7834e82fe4a8fb782f
Mungert69 <mungert@gmail.com>
2023-04-13 03:44:56 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/blog',
+  assetPrefix: '/blog/',
+  publicRuntimeConfig: {
+    basePath: "/blog",
+  },
+};
+
+module.exports = nextConfig;
